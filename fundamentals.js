7e0894f319498ffddc0a13eb6c66adbd7753de6c
Elizabeth Neary <20071724@mail.wit.ie>
2023-09-12 15:29:20 +0100

    Nested Objects

--- fundamentals.js
+++ fundamentals.js
@@ -1,7 +1,16 @@
 const aCar = {
     owner : "Joe Bloggs",
-    type : 'Toyota Corolla 1.8',
-    registration : '201WD1058'
+    type : {
+      make: 'Toyota',
+      model: 'Corolla',
+      cc: 1.8
+    },
+
+    registration : {
+      year: 201,
+      county_code: 'WD',
+      number: 1058
+    }
   };
   
-  console.log(aCar.owner);
+  console.log(aCar.owner + ' drives a ' + aCar.type.make + ' with registration: ' + aCar.registration)
